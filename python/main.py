adfeaafdf6d509d4a094dba65dcb6e10a78ec334
Tali de Mestre <tm8938@gadi-login-02.gadi.nci.org.au>
2022-03-10 14:46:58 +1100

    symlinking of model outputs

---
+++ python/main.py
@@ -0,0 +1,30 @@
+from argparse import ArgumentParser, Namespace
+import pathlib
+import os
+
+def setup():
+    parser = ArgumentParser(description='Generate transport matrices.')
+    parser.add_argument('-s', '--source', metavar='/path/to/model/run', 
+                        help='path to model run', required=True, type=pathlib.Path)
+    parser.add_argument('-o', '--output', metavar='/path/to/matrix/output', 
+                    help='path to output matrices', default="./matrix_output", type=pathlib.Path)
+    parser.add_argument('-m', '--model', metavar='"Model Type"', 
+                    help='input model type', default="MOM5")
+    return parser.parse_args()
+
+def main(args: Namespace):
+    # check for output directory, fail if already exists
+    args.output.mkdir(parents=False, exist_ok=False)
+
+    # create a symlink of all files in source directory in temp directory
+    tempdir = (args.output / ".temp")
+    tempdir.mkdir(parents=False, exist_ok=False)
+    os.system('ln -s ' + str(args.source) + '/* ' +str(tempdir))
+
+def teardown(args: Namespace):
+    os.system('rm -rf ' + str(args.output))
+
+
+args = setup()
+main(args)
+teardown(args)
